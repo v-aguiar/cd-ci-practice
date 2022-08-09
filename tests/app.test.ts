21827f19ded9e38a710391831eb3070ee129ca94
v-aguiar <v.aguiar.dev@gmail.com>
2022-08-09 18:22:46 -0300

    chore: add automated heroku deploy on push and run tests on PR

---
+++ tests/app.test.ts
@@ -0,0 +1,30 @@
+﻿import supertest from "supertest";
+import app from "../src/app.js";
+import prisma from "../src/database.js";
+
+const APP = supertest(app);
+
+beforeEach(async () => {
+  await prisma.student.deleteMany({});
+});
+
+describe("Student tests suite", () => {
+  it("should return status 201 and insert two students on db", async () => {
+    const students = [
+      {
+        name: "Fulano",
+      },
+      {
+        name: "Ciclano",
+      },
+    ];
+
+    const response = await APP.post("/students").send({ students });
+
+    expect(response.status).toBe(201);
+  });
+});
+
+afterAll(async () => {
+  await prisma.$disconnect();
+});
